1326c6ad707b8d72460ab56fbdf02250c979b26d
AlyRamzy <alyhassan62@yahoo.com>
2019-03-09 15:44:05 +0200

    post menu

--- app/src/main/java/com/example/android/minireddit/PosterAdapter.java
+++ app/src/main/java/com/example/android/minireddit/PosterAdapter.java
@@ -1,26 +1,36 @@
 package com.example.android.minireddit;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.PopupMenu;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 
+import java.lang.reflect.Method;
 import java.util.List;
+
 
 public class PosterAdapter extends ArrayAdapter<Post> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
         View ListItemView = convertView;
         if(ListItemView==null){//for making new List_item if there is no main one to change its data
             ListItemView= LayoutInflater.from(getContext()).inflate(R.layout.post_list_item,parent,false);
         }
         Post currentPost=getItem(position);// getting the current word in the arraylist
+
 
 
         ImageView postLogo=(ImageView)ListItemView.findViewById(R.id.postLogo);
@@ -50,7 +60,60 @@ public class PosterAdapter extends ArrayAdapter<Post> {
         postlikeCount.setText(String.valueOf(currentPost.getPostLikeCount()));
 
         TextView postCommentCount=(TextView)ListItemView.findViewById(R.id.postCommentCount);
-        postlikeCount.setText(String.valueOf(currentPost.getPostCommentCount()));
+        postCommentCount.setText(String.valueOf(currentPost.getPostCommentCount()));
+
+        ImageView menu=(ImageView) ListItemView.findViewById(R.id.postOptions);
+
+
+        menu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                switch (v.getId()) {
+                    case R.id.postOptions:
+
+                        PopupMenu popup = new PopupMenu(getContext(), v);
+                        popup.getMenuInflater().inflate(R.menu.post_menu,
+                                popup.getMenu());
+                        setForceShowIcon(popup);
+
+                        popup.show();
+                        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem item) {
+
+                                switch (item.getItemId()) {
+                                    case R.id.save:
+
+                                        //Or Some other code you want to put here.. This is just an example.
+                                        Toast.makeText(getContext(), " Install Clicked at position " + " : " , Toast.LENGTH_LONG).show();
+
+                                        break;
+                                    case R.id.blockUser:
+
+                                        Toast.makeText(getContext(), "Add to Wish List Clicked at position " + " : " , Toast.LENGTH_LONG).show();
+
+                                        break;
+
+                                    default:
+                                        break;
+                                }
+
+                                return true;
+                            }
+                        });
+
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+            }
+        });
 
 
 
@@ -60,6 +123,25 @@ public class PosterAdapter extends ArrayAdapter<Post> {
     public PosterAdapter(@NonNull Context context, @NonNull List<Post> objects) {
         super(context, 0,objects);
 
+    }
+    public static void setForceShowIcon(PopupMenu popupMenu) {
+        try {
+            Field[] fields = popupMenu.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                if ("mPopup".equals(field.getName())) {
+                    field.setAccessible(true);
+                    Object menuPopupHelper = field.get(popupMenu);
+                    Class<?> classPopupHelper = Class.forName(menuPopupHelper
+                            .getClass().getName());
+                    Method setForceIcons = classPopupHelper.getMethod(
+                            "setForceShowIcon", boolean.class);
+                    setForceIcons.invoke(menuPopupHelper, true);
+                    break;
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 
 
