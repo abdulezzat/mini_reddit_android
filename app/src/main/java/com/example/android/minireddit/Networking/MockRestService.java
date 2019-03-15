fe6674c9339117fa0814b5ef35354493c2c02e52
AlyRamzy <32557346+AlyRamzy@users.noreply.github.com>
2019-03-15 20:42:18 +0200

    MockService Structure

---
+++ app/src/main/java/com/example/android/minireddit/Networking/MockRestService.java
@@ -0,0 +1,17 @@
+package com.example.android.minireddit.Networking;
+
+import com.example.android.minireddit.datastructure.Post;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Aly on 3/15/2019.
+ */
+
+public class MockRestService implements Requests {
+
+    @Override
+    public ArrayList<Post> getTrendingPost() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/android/minireddit/Networking/RestService.java
@@ -0,0 +1,16 @@
+package com.example.android.minireddit.Networking;
+
+import com.example.android.minireddit.datastructure.Post;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Aly on 3/15/2019.
+ */
+
+public class RestService implements Requests {
+    @Override
+    public ArrayList<Post> getTrendingPost() {
+        return null;
+    }
+}
