156fa47ef1e843fbec020f16cdd61b0c076ab131
karashily <ahmed.elkarashily@gmail.com>
2019-03-17 23:04:13 +0200

    Profile Viewpager

--- app/src/main/java/com/example/android/minireddit/fragments/ProfileCommentsFragment.java
+++ app/src/main/java/com/example/android/minireddit/fragments/ProfileCommentsFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.minireddit.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -37,5 +38,21 @@ public class ProfileCommentsFragment extends Fragment {
         //listView.setAdapter(adapter);
 
         return rootView;
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
